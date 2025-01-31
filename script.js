a5110ab0d3139e768fe062cfc7061b94c9f12ac3
DachepallySahithi <130071155+DachepallySahithi@users.noreply.github.com>
2025-01-31 18:28:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,3 +7,33 @@ const images = [
   { url: "https://picsum.photos/id/238/200/300" },
   { url: "https://picsum.photos/id/239/200/300" },
 ];
+function loadImage(image) {
+	return new Promise((resolve, reject) =>{
+		const img = new Image();
+		img.src = image.url;
+		img.onload = () => resolve(img);
+		img.onerror = () => reject(`Failed to load image's URL: ${image.url}`);
+	});
+}
+
+function downloadImage() {
+	output.innerHTML = "";
+	const loading = document.createElement("div");
+	loading.id = "loading";
+	loading.innerText = "Loading...";
+	output.appendChild(loading);
+
+    Promise.all(images.map(loadImage))
+    .then((loadedImages) => {
+    loading.remove();
+		loadedImages.forEach((img) => output.appendChild(img));
+	})
+	.catch((error) => {
+		loading.remove();
+		const errorDiv = document.createElement("div");
+		errorDiv.id = "error";
+		errorDiv.innerText = error;
+	    output.appendChild(errorDiv);
+	});
+}
+btn.addEventListener("click", downloadImages);
